e22afaa676aefe3a8a889dbfc15f73fba9a91fcf
CodingBySahil <muhammadsahil345737@gmail.com>
2024-03-28 05:02:59 -0700

    second class of typeScript

---
+++ ts02/index.ts
@@ -0,0 +1,16 @@
+// // // var a
+// // console.log(a)
+// // let a = 12
+// // console.log(a)
+
+// let numbers = [0, 1, 2, 3, 4, 5];
+
+// // numbers.push(7)  // insert 7 to numbers
+// // numbers.pop()   // remove last index value
+// // numbers.shift(10) // remove value from last
+// // numbers.unshift(10)   // add value to index 0
+// // numbers.splice(3, 12); // start removing from index 3 and remove 12 values after index 3 it will also remove index 3
+// numbers.splice(1);
+// // numbers.splice(-1,2)
+// // numbers.splice(-1,3)
+// console.log(numbers);
