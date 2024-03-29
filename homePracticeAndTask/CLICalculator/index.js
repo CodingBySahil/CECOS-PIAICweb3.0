ab3e1e8ac6aec08a93075b81022c23c73f3ef3d3
CodingBySahil <muhammadsahil345737@gmail.com>
2024-03-28 22:48:10 -0700

    CLI calculator task practise

---
+++ homePracticeAndTask/CLICalculator/index.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var inquirer_1 = require("inquirer");
+var answer = await inquirer_1.default.prompt([
+    { message: "Enter first number", type: "number", name: "num1" },
+    { message: "Enter second number", type: "number", name: "num2" },
+    {
+        message: "Select your desire operator",
+        type: "list",
+        name: "num1",
+        choices: ["addition", "subtraction", "multiplication", "division"],
+    },
+]);
+if (answer.operator == "addition") {
+    console.log(answer.num1 + answer.num2);
+}
+else if (answer.operator == "subtraction") {
+    console.log(answer.num1 - answer.num2);
+}
+else if (answer.operator == "multiplication") {
+    console.log(answer.num1 * answer.num2);
+}
+else if (answer.operator == "division") {
+    console.log(answer.num1 / answer.num2);
+}
